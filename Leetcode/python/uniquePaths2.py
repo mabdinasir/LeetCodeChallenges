e617a52398393210c3eeef917d9f67d2efb4dc2b
Mohamed Abdinasir <mabdinasira@gmail.com>
2024-03-13 22:17:58 +1100

    mix of sql and py solutions

---
+++ Leetcode/python/uniquePaths2.py
@@ -0,0 +1,42 @@
+# You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+# Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+# Example 1:
+# Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+# Output: 2
+
+# Explanation: There is one obstacle in the middle of the 3x3 grid above.
+# There are two ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down -> Down
+# 2. Down -> Down -> Right -> Right
+
+from typing import List
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0 for i in range(n)] for j in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                elif i == 0 and j == 0:
+                    dp[i][j] = 1
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+
+print(Solution().uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])) # 2
+print(Solution().uniquePathsWithObstacles([[0,1],[0,0]])) # 1
+print(Solution().uniquePathsWithObstacles([[0,0],[0,1]])) # 0
+print(Solution().uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0],[0,0,0]])) # 4
