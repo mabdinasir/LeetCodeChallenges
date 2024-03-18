c37cec6877af30f6a511d3e9ac556119713f2ad4
Mohamed Abdinasir <mabdinasira@gmail.com>
2024-03-18 22:01:16 +1100

    query quality, last to fit in bus, sort colors, ...

---
+++ Leetcode/python/minWindowSubstring.py
@@ -0,0 +1,46 @@
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+# The testcases will be generated such that the answer is unique.
+
+# Example 1:
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+# Example 2:
+# Input: s = "a", t = "a"
+# Output: "a"
+# Explanation: The entire string s is the minimum window.
+
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s, t):
+        if not s or not t:
+            return ""
+        dict_t = Counter(t)
+        required = len(dict_t)
+        l, r = 0, 0
+        formed = 0
+        window_counts = {}
+        ans = float("inf"), None, None
+        while r < len(s):
+            character = s[r]
+            window_counts[character] = window_counts.get(character, 0) + 1
+            if character in dict_t and window_counts[character] == dict_t[character]:
+                formed += 1
+            while l <= r and formed == required:
+                character = s[l]
+                if r - l + 1 < ans[0]:
+                    ans = (r - l + 1, l, r)
+                window_counts[character] -= 1
+                if character in dict_t and window_counts[character] < dict_t[character]:
+                    formed -= 1
+                l += 1    
+            r += 1    
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+sol = Solution()
+print(sol.minWindow("ADOBECODEBANC", "ABC")) # "BANC"
+print(sol.minWindow("a", "a")) # "a"
