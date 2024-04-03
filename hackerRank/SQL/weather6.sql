d91024390a1b0e51308f320297cae9e7f6133bed
Mohamed Abdinasir <mabdinasira@gmail.com>
2024-04-04 09:18:37 +1100

    aggregates

--- hackerRank/SQL/weather6.sql
+++ hackerRank/SQL/weather6.sql
@@ -87,3 +87,38 @@ WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
 SELECT ROUND(MIN(LAT_N), 4)
 FROM STATION
 WHERE LAT_N > 38.7780;
+
+17. Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+-- Solution
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);
+
+18. Query the names of the country where the Northernmost Northern Latitude (LAT_N) meets the Westernmost Western Longitude (LONG_W) in the STATION table.
+-- Solution
+SELECT CITY
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION)
+AND LONG_W = (SELECT MIN(LONG_W) FROM STATION);
+
+19. Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+-- Solution
+SELECT ROUND(ABS(MAX(LAT_N) - MIN(LAT_N)) + ABS(MAX(LONG_W) - MIN(LONG_W)), 4)
+FROM STATION;
+
+20. Query the Euclidean Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+-- Solution
+SELECT ROUND(SQRT(POW(MAX(LAT_N) - MIN(LAT_N), 2) + POW(MAX(LONG_W) - MIN(LONG_W), 2)), 4)
+FROM STATION;
+
+21. A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
+-- Solution
+SELECT ROUND(LAT_N, 4)
+FROM STATION
+ORDER BY LAT_N
+LIMIT 1 OFFSET (SELECT COUNT(LAT_N) / 2 FROM STATION);
